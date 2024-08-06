352a445f97ee3cd32ec6d86e253495f9fcfab087
rofik <rofik@newtronic-solution.com>
2024-08-06 13:38:14 +0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, JetBrains_Mono, Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+export const jetbrainsMono = JetBrains_Mono({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
