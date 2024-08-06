2d5b3f7ca83e1995188fddda2509403e70f9bb7c
rofik <rofik@newtronic-solution.com>
2024-08-06 19:07:23 +0700

    feat: cards skeleton

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import React, {Suspense} from "react";
-import {LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
+import {CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
 
 export default async function Page() {
 
@@ -13,7 +13,9 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <CardWrapper/>
+                <Suspense fallback={<CardsSkeleton/>}>
+                    <CardWrapper/>
+                </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton/>}>
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,8 +1,6 @@
 import {BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon,} from '@heroicons/react/24/outline';
 import {lusitana} from '@/app/ui/fonts';
 import {fetchCardData} from "@/app/lib/data";
-import {Suspense} from "react";
-import {CardSkeleton} from "@/app/ui/skeletons";
 
 const iconMap = {
     collected: BanknotesIcon,
@@ -43,17 +41,15 @@ export function Card({title, value, type,}:
 
     return (
         <>
-            <Suspense fallback={<CardSkeleton/>}>
-                <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-                    <div className="flex p-4">
-                        {Icon ? <Icon className="h-5 w-5 text-gray-700"/> : null}
-                        <h3 className="ml-2 text-sm font-medium">{title}</h3>
-                    </div>
-                    <p className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}>
-                        {value}
-                    </p>
+            <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+                <div className="flex p-4">
+                    {Icon ? <Icon className="h-5 w-5 text-gray-700"/> : null}
+                    <h3 className="ml-2 text-sm font-medium">{title}</h3>
                 </div>
-            </Suspense>
+                <p className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}>
+                    {value}
+                </p>
+            </div>
         </>
     );
 }
