6752a11e25365e2fa868d0b5d50b4ca0080f6561
rofik <rofik@newtronic-solution.com>
2024-08-07 22:07:11 +0700

    feat: delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,6 @@ import {z} from 'zod';
 import {sql} from "@vercel/postgres";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
-import {fetchInvoiceById} from "@/app/lib/data";
 
 const InvoiceSchema = z.object({
     id: z.string(),
@@ -25,9 +24,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    sql`
+    try {
+        await sql`
         insert into invoices(customer_id, amount, status, date) values (${customerId}, ${amountInCents}, ${status}, ${date})
        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        }
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
@@ -43,14 +48,34 @@ export async function updateInvoice(id: string, formData: FormData) {
     })
     const amountInCents = amount * 100;
 
-    sql`
+    try {
+
+        await sql`
         update invoices
         set customer_id = ${customerId},
             amount = ${amountInCents},
             status = ${status}
         where id = ${id}
         `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Update Invoice.',
+        }
+    }
 
     revalidatePath(`/dashboard/invoices`);
     redirect(`/dashboard/invoices`);
+}
+
+export async function deleteInvoiceById(id: string) {
+    try {
+        await sql`
+        delete from invoices where id = ${id}
+       `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Delete Invoice.',
+        }
+    }
+    revalidatePath(`/dashboard/invoices`);
 }
